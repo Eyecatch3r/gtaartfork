87b5274d68cc553f65ca822527697c5217816a3b
Aldo Costa <aldo.costa@web.de>
2023-07-14 00:44:20 +0200

    -added Header

--- app/page.js
+++ app/page.js
@@ -4,10 +4,11 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-        <>
+        <div>
+            <h1 className={styles['glowing-header']}>GTA Artwork Generator</h1>
             <script type="module" src="https://gradio.s3-us-west-2.amazonaws.com/3.36.1/gradio.js" async />
             <gradio-app info="false" src="https://eyecatcher-itsjayqz-gta5-artwork-diffusion.hf.space"></gradio-app>
-        </>
+        </div>
     </main>
   )
 }
