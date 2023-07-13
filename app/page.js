8a9e5de225ebbede8141cf4364b62f58f6ca96e2
Aldo Costa <aldo.costa@web.de>
2023-07-13 22:31:16 +0200

    -init

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
+'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
-
 export default function Home() {
   return (
     <main className={styles.main}>
-      <script type="module"
-              src="https://gradio.s3-us-west-2.amazonaws.com/3.36.1/gradio.js">
-      </script>
-        <gradio-app src="models/ItsJayQz/GTA5_Artwork_Diffusion"></gradio-app>
+        <>
+            <script type="module"
+                    src="https://gradio.s3-us-west-2.amazonaws.com/3.36.1/gradio.js">
+            </script>
+            <gradio-app src="https://eyecatcher-itsjayqz-gta5-artwork-diffusion.hf.space"></gradio-app>
+        </>
     </main>
   )
 }
