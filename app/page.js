516f3a80bc7d2dba62d16a0020686e2a5572a73a
Aldo Costa <aldo.costa@web.de>
2023-07-14 00:53:23 +0200

    -dropshadow fix

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
         <div>
-            <h1 className={styles['glowing-header']}>GTA Artwork Generator</h1>
+            <h1 className="transition filter duration-500 hover:drop-shadow-[0_7px_11px_rgba(100,100,100,1)]">GTA Artwork Generator</h1>
             <script type="module" src="https://gradio.s3-us-west-2.amazonaws.com/3.36.1/gradio.js" async />
             <gradio-app info="false" src="https://eyecatcher-itsjayqz-gta5-artwork-diffusion.hf.space"></gradio-app>
         </div>
