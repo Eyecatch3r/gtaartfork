d7cb644e81697171bbba257dd02d1ecea19505f7
Aldo Costa <aldo.costa@web.de>
2023-07-13 22:33:34 +0200

    -made async

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
         <>
-            <script type="module"
+            <script async={} type="module"
                     src="https://gradio.s3-us-west-2.amazonaws.com/3.36.1/gradio.js">
             </script>
             <gradio-app src="https://eyecatcher-itsjayqz-gta5-artwork-diffusion.hf.space"></gradio-app>
